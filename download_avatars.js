4632b0b5d0d877d1f7e4a03bd77c0a78b0f1c28f
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-04 21:27:56 +0000

    Modified authorization token and modified getRepoContributors to call downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -26,7 +26,7 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Error: ", err);
 
   result.forEach( function(contributor) {
-    console.log(contributor['avatar_url']);
+    downloadImageByURL(contributor['avatar_url'] + "avatars/", contributor['login'] + ".jpg");
   });
 })
 
@@ -37,14 +37,10 @@ function downloadImageByURL(url, filePath) {
       throw err;
     })
     .on('response', function(response) {
-      console.log("Content type:", response.headers['content-type']);
-      console.log("Downloading image...");
+      console.log("Downloading image:", filePath);
     })
-    .pipe(fs.createWriteStream('./avatar.jpg'))
+    .pipe(fs.createWriteStream("./avatars/" + filePath))
     .on('finish', function() {
         console.log("Download complete.")
     });
 }
-
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
-
