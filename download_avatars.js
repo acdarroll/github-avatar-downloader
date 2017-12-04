eb26d087e6bb1a6275acef2ef64263736e17644d
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-04 22:43:45 +0000

    Fixes for url and filePath formatting

--- download_avatars.js
+++ download_avatars.js
@@ -6,18 +6,17 @@ var args = process.argv.slice(2);
 
 console.log("You're using the Github avatar downloader!");
 
+
+// filePath is the path to a local directory, url is the user avatar url
 function downloadImageByURL(url, filePath) {
 
-  request.get(url + '/' + filePath)
+  request.get(url)
     .on('error', function(err) {
       throw err;
     })
-    .on('response', function(response) {
-      console.log("Downloading image:", filePath);
-    })
-    .pipe(fs.createWriteStream("./avatars/" + filePath))
+    .pipe(fs.createWriteStream(filePath))
     .on('finish', function() {
-        console.log("Download complete.")
+        console.log("Finished downloading image:", filePath);   // Output the filepath when the download is finished
     });
 }
 
@@ -25,25 +24,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
-      "User-Agent": "acdarroll",
-      "Authorization": "token " + secrets.GITHUB_TOKEN
+      "User-Agent": "request",
+      "Authorization": `token ${secrets.GITHUB_TOKEN}`
     }
   }
 
   request(options, function(err, res, body) {
     var contributors = JSON.parse(body);
-    console.log(res.headers);
-    console.log(contributors);
+    console.log("Response status code:", res.statusCode);
 
     cb(err, contributors);
   });
 }
 
+// Callback function will download the avatar for each contributor listed in the repo
 getRepoContributors(args[0], args[1], function(err, result) {
   console.log("Error: ", err);
 
   result.forEach( function(contributor) {
-    downloadImageByURL(contributor['avatar_url'] + "avatars/", contributor['login'] + ".jpg");
+    downloadImageByURL(contributor['avatar_url'], `./avatars/${contributor['login']}.jpg`);
   });
 })
 
