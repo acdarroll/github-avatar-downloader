6bc2a6ae492105f7b5ade69a7213a9b086c86c0c
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-04 21:13:19 +0000

    Added a function that downloads a given avatar

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require("fs");
 
 console.log('Welcome to the Github avatar downloader!');
 
@@ -7,12 +8,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
-      "User-Agent": `acdarroll:${secrets.GITHUB_URL}`
+      "User-Agent": "acdarroll",
+      "Authorization": "token " + secrets.GITHUB_TOKEN
     }
   }
 
   request(options, function(err, res, body) {
     var contributors = JSON.parse(body);
+    console.log(res.headers);
+    console.log(contributors);
 
     cb(err, contributors);
   });
@@ -25,4 +29,22 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log(contributor['avatar_url']);
   });
 })
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url + '/' + filePath)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log("Content type:", response.headers['content-type']);
+      console.log("Downloading image...");
+    })
+    .pipe(fs.createWriteStream('./avatar.jpg'))
+    .on('finish', function() {
+        console.log("Download complete.")
+    });
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
 
