789e8a57033c875445bfcfa88b88330e72bacdb4
Andrew Carroll <andrewjcarroll3@gmail.com>
2017-12-04 19:48:24 +0000

    Added authentication via token and print avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,28 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 console.log('Welcome to the Github avatar downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      "User-Agent": `acdarroll:${secrets.GITHUB_URL}`
+    }
+  }
 
+  request(options, function(err, res, body) {
+    var contributors = JSON.parse(body);
+
+    cb(err, contributors);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Error: ", err);
-  console.log("Result: ", result);
+
+  result.forEach( function(contributor) {
+    console.log(contributor['avatar_url']);
+  });
 })
+
